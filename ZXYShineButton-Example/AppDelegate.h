999cc43d61db35071930969064b29f4dabf009fd
赵阳 <zhaoyang@zhaoyangdeMBP.lan>
2017-12-27 23:03:35 +0800

    1.0.4

---
+++ ZXYShineButton-Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXYShineButton-Example
+//
+//  Created by 赵阳 on 2017/12/27.
+//  Copyright © 2017年 赵阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
